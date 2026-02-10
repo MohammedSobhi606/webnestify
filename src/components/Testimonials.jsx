8bdfa273b2199c0e469436dbfbf9c33940d1f1c9
MohammedSobhi606 <Mohammmedsobhi606>
2026-02-10 14:33:24 +0200

    image edit

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,15 +4,12 @@ import simonImg from "../assets/simon.webp";
 export default function Testimonials() {
   return (
     <section className="py-16">
-      <div className="grid grid-cols-1 md:grid-cols-2  rounded-xl  bg-neutral-900">
-        <div className="overflow-clip rounded-xl border-4 border-neutral-700">
-          <img
-            src={simonImg}
-            alt="test"
-            className="aspect-video object-cover hover:scale-110 transition-transform ease-in-out"
-          />
-        </div>
-
+      <div className="grid grid-cols-1 md:grid-cols-2  rounded-xl  bg-neutral-900 overflow-hidden">
+        <img
+          src={simonImg}
+          alt="test"
+          className="aspect-video object-cover rounded-xl border-4 border-neutral-700 hover:scale-110 transition-transform ease-in-out"
+        />
         <div className="grid gap-2 p-8 ">
           <p>{testimonial.quote}</p>
           <div className="space-y-0">
